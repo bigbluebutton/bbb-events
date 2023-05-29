c766c31599a193c1124d0f847c73316b32af6b62
Richard Alam <ralam@blindsidenetworks.com>
2023-05-29 21:13:56 +0000

    - add test to poll published event

--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
+require "bbbevents"
 
 RSpec.describe BBBEvents::Poll do
   before(:all) do
-    @poll = @sample.polls.first
+    @sample1 = BBBEvents.parse(file_fixture("new-events-2_6.xml"))
+    @poll = @sample1.polls.first
   end
 
   context "#id" do
     it "should properly parse id." do
-      expect(@poll.id).to eq("d2d9a672040fbde2a47a10bf6c37b6a4b5ae187f-1534433961845/1/1534434041159")
+      expect(@poll.id).to eq("bd5a59a3f61d1ec7402cf9105a090e84f867421f-1684349961676/1/1684349997071")
     end
   end
 
@@ -26,8 +28,8 @@ RSpec.describe BBBEvents::Poll do
   end
 
   context "#published?" do
-    it "has published? alias." do
-      expect(@poll).to respond_to(:published?)
+    it "poll has been published" do
+      expect(@poll.published).to be true
     end
   end
 end
