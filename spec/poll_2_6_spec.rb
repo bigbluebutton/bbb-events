cb9251a68693a878193722895391275d648c6e06
Richard Alam <ritzalam@gmail.com>
2023-06-07 15:32:33 -0400

    In 2.6, polls are not set to published in the generated data (#30)
    In 2.5, the event to indicate that the poll has been published is the AddShapeEvent.
    This event has been removed from 2.6 when whiteboard was changed to tldraw. Now we need a different event (PollPublishedRecordEvent) to indicate that the poll has been publish.
    
    * add new event to indicate poll has been published
    * add test to poll published event

--- spec/poll_2_6_spec.rb
+++ spec/poll_2_6_spec.rb
@@ -1,15 +1,14 @@
 require "spec_helper"
-require "bbbevents"
 
 RSpec.describe BBBEvents::Poll do
   before(:all) do
-    @sample = BBBEvents.parse(file_fixture("new-events-2_5.xml"))
+    @sample = BBBEvents.parse(file_fixture("new-events-2_6.xml"))
     @poll = @sample.polls.first
   end
 
   context "#id" do
     it "should properly parse id." do
-      expect(@poll.id).to eq("4273443ce696f01108e3addc86386d73329b8d98-1684351990846/1/1684352053505")
+      expect(@poll.id).to eq("bd5a59a3f61d1ec7402cf9105a090e84f867421f-1684349961676/1/1684349997071")
     end
   end
 
