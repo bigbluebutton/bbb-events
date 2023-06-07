ed91d4a119a429de5c6ee3a097b382048b4cf69f
Richard Alam <ritzalam@gmail.com>
2023-06-07 19:26:33 +0000

    - remove unused spec file

--- spec/poll_2_6_spec.rb
+++ spec/poll_2_6_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 
 RSpec.describe BBBEvents::Poll do
   before(:all) do
+    @sample = BBBEvents.parse(file_fixture("new-events-2_6.xml"))
     @poll = @sample.polls.first
   end
 
   context "#id" do
     it "should properly parse id." do
-      expect(@poll.id).to eq("d2d9a672040fbde2a47a10bf6c37b6a4b5ae187f-1534433961845/1/1534434041159")
+      expect(@poll.id).to eq("bd5a59a3f61d1ec7402cf9105a090e84f867421f-1684349961676/1/1684349997071")
     end
   end
 
